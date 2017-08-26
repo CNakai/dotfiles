aa61d5788dc6aa8b4324e1bb37967e046f36b9f8
Colter Nakai McCarty <cnm62@nau.edu>
2017-08-26 14:26:58 -0700

    first commit

---
+++ .emacs.d/init/load_all.el
@@ -0,0 +1,4 @@
+(load "~/.emacs.d/init/basic_editor_settings.el")
+(load "~/.emacs.d/init/evil_and_related_packages.el")
+(load "~/.emacs.d/init/programming_language_integration_packages.el")
+(load "~/.emacs.d/init/misc_packages.el")
---
+++ .emacs.d/init/misc_packages.el
@@ -0,0 +1,8 @@
+(use-package nlinum-relative
+  :ensure t)
+
+(use-package restart-emacs
+  :ensure t)
+
+(use-package fill-column-indicator
+  :ensure t)
